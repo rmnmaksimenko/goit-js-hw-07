692e85595ccabe9c49929273118f04bca9f21f33
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-05-05 16:47:56 +0300

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkup = [...galleryItems]
+  .map(image => {
+    const imageSmall = image.preview;
+    const imageLarge = image.original;
+    const description = image.description;
+    return `<div class="gallery__item">
+            <a class="gallery__link" href="${imageLarge}">
+                <img
+                class="gallery__image"
+                src="${imageSmall}"
+                data-source="${imageLarge}"
+                alt="${description}"
+                />
+            </a>
+        </div>`;
+  })
+  .join('');
+
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery.addEventListener('click', selectImage);
+
+function selectImage(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  showImage(e.target.getAttribute(['data-source']));
+}
+
+function showImage(image) {
+  const instance = basicLightbox.create(`
+      <img src="${image}" width="800" height="600">
+  `);
+
+  instance.show();
+}
