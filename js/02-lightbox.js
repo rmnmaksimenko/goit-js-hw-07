34c1e072c3747717a717b2956b4f8fca83e0313a
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-06-02 14:16:51 +0300

    lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryMarkup = [...galleryItems]
+  .map(image => {
+    const imageSmall = image.preview;
+    const imageLarge = image.original;
+    const description = image.description;
+    return `<div class="gallery__item">
+            <a class="gallery__link" href="${imageLarge}">
+                <img
+                class="gallery__image"
+                src="${imageSmall}"
+                data-source="${imageLarge}"
+                alt="${description}"
+                />
+            </a>
+        </div>`;
+  })
+  .join('');
+
+const galleryHTML = document.querySelector('.gallery');
+galleryHTML.insertAdjacentHTML('beforeend', galleryMarkup);
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
